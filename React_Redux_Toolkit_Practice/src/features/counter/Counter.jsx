6510d50aeca3c766fd7aa5d7e9860a2c2fd71003
Aryan Chettri <aryanchettri17@gmail.com>
2025-09-02 02:18:44 +0530

    This is the first project using React Redux Toolkit and this demonstrates a Counter

---
+++ React_Redux_Toolkit_Practice/src/features/counter/Counter.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+
+export function Counter(){
+
+    const count = useSelector((state)=> state.counter.value)
+    const disptch = useDispatch();
+    const [value, setValue] = useState(0);
+
+    return(
+        <div>
+            <div>
+                <button
+                    aria-label="Increment Value"
+                    onClick={()=>disptch(increment())}
+                >
+                    Increment
+                </button>
+                <span> {count} </span>
+                <button 
+                    aria-label="Decrement Value"
+                    onClick={()=> disptch(decrement())}
+                >
+                    decrement
+                </button>
+                
+            </div>
+        </div>
+    )
+}
