46d0aecccfc14df02caeeae4320fb5cb6aa54496
Aryan Chettri <aryanchettri17@gmail.com>
2025-09-30 21:05:23 +0530

    This is the commit that involves the fetching of values

--- appwritePractice3/pages/Home.jsx
+++ appwritePractice3/pages/Home.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 
+import { useState, useEffect } from 'react'
+
+import { databases } from '../appwrite/config.js'
+
+
 function Home() {
+
+    const [array, setArray] = useState([]);
+
+    useEffect(()=>{
+        init();
+    },[])
+
+    const init = async() =>{
+
+        try {
+
+            const data = await databases.listDocuments(
+
+            import.meta.env.VITE_APPWRITE_DATABASE_ID,
+            import.meta.env.VITE_APPWRITE_COLLECTION_ID
+        );
+
+        setArray(data.documents);
+
+        console.log(data.documents);
+            
+        } catch (error) {
+
+
+            console.log("Init error:", error);
+        }
+    }
+
   return (
     <div>
-      <h1>Home page</h1>
+      {array.map((element, index)=>(
+
+        <h1 key={index}>
+
+          {element.email}
+          {element.age}
+        </h1>
+      ))}
     </div>
   )
 }
