f732212f17bf95219d038247c4d6b2f888edc5b3
Aryan Chettri <aryanchettri17@gmail.com>
2025-09-30 20:19:59 +0530

    This is just a random practice project of appwrite

---
+++ appwritePractice3/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+    
+    <Routes>
+
+      <Route element={<Home/>} path='/'/>
+      <Route element={<Login/>} path='/login'/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
