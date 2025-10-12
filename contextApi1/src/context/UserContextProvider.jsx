e7da2fe1c3a23b9d67aa24c44ecba15b14262c48
Aryan Chettri <aryanchettri17@gmail.com>
2025-10-12 22:04:57 +0530

    Practice session of context APIs

---
+++ contextApi1/src/context/UserContextProvider.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import React from 'react'
+
+import UserContext from './UserContext'
+
+function UserContextProvider({children}) {
+  
+    const [user, setUser] = React.useState(null);
+
+    return(
+        <UserContext.Provider value={{user, setUser}}>
+
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider
